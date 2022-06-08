bba8dc123cd16ee1b47ad3f2bbd29e8ec078b07f
Erkki Rasi <erkkirasi@gmail.com>
2022-06-08 10:44:25 +0300

    created data.txt and feature to append user input to data.txt

--- Shopping List/Program.cs
+++ Shopping List/Program.cs
@@ -1,13 +1,37 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.IO;
+/*Asks user one shoppping item
+* Saves input to a file
+*  Hint: append
+*  
+*  Bonus: On startup, print all items from the file*/
 namespace Shopping_List
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string polku = @"C:\Users\Erkki\source\repos\Shopping List\Shopping List\text.txt";
+
+            //List<string> tuotteet = new List<string>();
+
             Console.Write("Kirjoita tuote lisättäväksi ostoslistaan: ");
             string syote = Console.ReadLine();
+
+            if (File.Exists(polku))
+            {
+                File.WriteAllText(polku, syote + Environment.NewLine);
+            }
+            else
+            {
+                Console.WriteLine("Tiedostoa ei ole olemassa!");
+            }
+            
+
+
+
+
         }
     }
 }
