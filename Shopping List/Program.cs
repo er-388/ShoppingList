726dcba898652bfd9f5d40465617d8d77d3c0fc9
Erkki Rasi <erkkirasi@gmail.com>
2022-06-08 10:16:15 +0300

    project started

---
+++ Shopping List/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Shopping_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kirjoita tuote lisättäväksi ostoslistaan: ");
+            string syote = Console.ReadLine();
+        }
+    }
+}
